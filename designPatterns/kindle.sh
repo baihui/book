e7eed6876fdff9d7107382a03bf2dc63ffacf589
baihui <baihuiit@163.com>
2019-11-09 20:47:58 +0800

    publish

---
+++ designPatterns/kindle.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "解析:\n"
+bookfile=$(gitbook parse|grep 'Book located in'|sed 's/ //g')
+bookfile="${bookfile##*/}"
+
+echo "获取书名:\n${bookfile}"
+gitbook install
+echo "安装插件:\n${bookfile}"
+gitbook build
+echo "build:\n${bookfile}"
+bookp=~/books
+
+if [ ! -d ${bookp} ]; then
+  mkdir ${bookp}
+  echo "目录创建完成${bookp}"
+fi
+
+gitbook mobi ./ ${bookp}/${bookfile}.mobi
+echo "创建完成:\n${bookp}/${bookfile}.mobi" 
